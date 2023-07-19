698818f56e05e02e9e5df40194b22aa4079a58b1
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-07-18 20:18:18 -0500

    added basic html

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Sign Up</title>
+    <lunk rel="stylesheet" href="style.css" />
+    
+    </head>
     <body>
+    <h1>Sign Up</h1>
+  <form action="signup.php" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br>
 
-        <?php
-        echo "My first PHP script!" ;
-        ?>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br>
+
+    <input type="submit" value="Sign Up">
+  </form>
+            
+        </div>
+        <script src="script.js"></script>
     </body>
 </html>
